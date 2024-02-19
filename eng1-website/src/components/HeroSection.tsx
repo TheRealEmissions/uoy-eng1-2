64987f10a699326c392834524cdb9f31538992c5
charliepiper <charliepiperr@gmail.com>
2024-02-19 15:39:26 +0000

    fix: responsiveness

--- eng1-website/src/components/HeroSection.tsx
+++ eng1-website/src/components/HeroSection.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <section id="Hero Section" className="pb-32">
-        <div className = "container md:mt-20 mt-12 xl:p-24 md:p-24 lg:p-24 p-10 flex gap-20 flex justify-center items-center">
+        <div className = "container md:mt-20 mt-12 xl:p-24 md:p-24 lg:p-24 p-10 flex gap-20 flex ">
             <div className="md:pt-10 pt-1">
                 <div className="max-w-xl md:max-w-3xl">
-                    <p className="text-2xl">hello!</p>
-                    <h2 className="font-semibold text-5xl ">We're <span className=" font-bold">/cohort1/group1</span></h2>
-                    <p className = "text-base mt-4 mb-6">
+                    <p className="text-xl md:text-2xl">hello!</p>
+                    <h2 className="font-semibold text-4xl  md:text-5xl ">We're <span className=" font-bold">/cohort1/group1</span></h2>
+                    <p className = "text-sm md:text-base mt-4 mb-6">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi commodo tellus sit amet magna tempor vestibulum. Sed 
                         <span className=" font-bold"> lobortis laoreet sem,</span> ut laoreet erat aliquam non. 
                     </p>
