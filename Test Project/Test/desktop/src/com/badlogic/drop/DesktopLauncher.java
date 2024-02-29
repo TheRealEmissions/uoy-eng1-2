6090d9e8118cbf202227b3e15fe3295064b31546
Tom Haslam <160120113+th1583@users.noreply.github.com>
2024-02-29 10:42:25 +0000

    test

--- Test Project/Test/desktop/src/com/badlogic/drop/DesktopLauncher.java
+++ Test Project/Test/desktop/src/com/badlogic/drop/DesktopLauncher.java
@@ -4,7 +4,8 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.drop.Drop;
 
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument temp stuff here
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+// temp stuff here
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
