5dbed741f453021044052857c24381d3b7ffeb9a
charliepiper <charliepiperr@gmail.com>
2024-02-29 16:16:38 +0000

    Added Menu screens

--- game/core/src/com/eng1/game/ENG1.java
+++ game/core/src/com/eng1/game/ENG1.java
@@ -1,31 +1,58 @@
 package com.eng1.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import eng1.model.views.*;
 
-public class ENG1 extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class ENG1 extends Game {
+	private LoadingScreen loadingScreen;
+	private PreferencesScreen preferencesScreen;
+	private MenuScreen menuScreen;
+	private MainScreen mainScreen;
+	private EndScreen endScreen;
+	private AppPreferences preferences;
+
+	public final static int MENU = 0;
+	public final static int PREFERENCES = 1;
+	public final static int APPLICATION = 2;
+	public final static int ENDGAME = 3;
+
+
+
+
+
+	public void changeScreen(int screen){
+		switch(screen){
+			case MENU:
+				if(menuScreen == null) menuScreen = new MenuScreen(this); // added (this)
+				this.setScreen(menuScreen);
+				break;
+			case PREFERENCES:
+				if(preferencesScreen == null) preferencesScreen = new PreferencesScreen(this); // added (this)
+				this.setScreen(preferencesScreen);
+				break;
+			case APPLICATION:
+				if(mainScreen == null) mainScreen = new MainScreen(this); //added (this)
+				this.setScreen(mainScreen);
+				break;
+			case ENDGAME:
+				if(endScreen == null) endScreen = new EndScreen(this);  // added (this)
+				this.setScreen(endScreen);
+				break;
+		}
+	}
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		loadingScreen = new LoadingScreen(this);
+		setScreen(loadingScreen);
+
+		preferences = new AppPreferences();
+
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public AppPreferences getPreferences(){
+		return this.preferences;
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+
 }
---
+++ game/core/src/eng1/model/views/LoadingScreen.java
@@ -0,0 +1,48 @@
+package eng1.model.views;
+
+import com.badlogic.gdx.Screen;
+import com.eng1.game.ENG1;
+
+public class LoadingScreen implements Screen {
+    private ENG1 parent; // a field to store our orchestrator
+
+    // our constructor with a ENG1 argument
+    public LoadingScreen(ENG1 eng1){
+        parent = eng1;     // setting the argument to our field.
+    }
+    @Override
+    public void show() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void render(float delta) {
+        // TODO Auto-generated method stub
+        parent.changeScreen(ENG1.MENU);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void hide() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+    }
+}
