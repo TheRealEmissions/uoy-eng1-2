f1dace652703f529d62e78491d45244679472ca5
charliepiper <charliepiperr@gmail.com>
2024-03-02 13:21:42 +0000

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-// next.config.js
-module.exports = {
-  // Export the site to static HTML
-  // https://nextjs.org/docs/advanced-features/static-html-export
-  // Use generateStaticParams() to define the pages you want to export
-  async generateStaticParams() {
-    return {
-      '/': { page: '/' }
-      // Add additional pages here if needed
-    };
-  }
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  favicon: "/icon.ico",
+  output: "export",
+  images: {
+      unoptimized: true
+  },
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
