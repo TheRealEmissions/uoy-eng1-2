83bb0312ecce64c787588129cb4deab2528b3c07
Sammy <git@sammyhori.com>
2024-04-25 14:55:02 +0100

    Initial Asset Tests, testing that the character's assets appear

---
+++ game/tests/src/eng1/project/gdxtesting/AssetTests.java
@@ -0,0 +1,29 @@
+package eng1.project.gdxtesting;
+
+import static org.junit.Assert.assertTrue;
+
+import com.eng1.game.Play;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.badlogic.gdx.Gdx;
+
+import eng1.project.gdxtesting.GdxTestRunner;
+
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+    @Test
+    public void testCharacter1AssetExists() {
+        assertTrue("Testing whether the asset for character 1 exists", Gdx.files.internal(Play.CHAR1).exists());
+    }
+
+    @Test
+    public void testCharacter2AssetExists() {
+        assertTrue("Testing whether the asset for character 2 exists", Gdx.files.internal(Play.CHAR2).exists());
+    }
+
+    @Test
+    public void testCharacter3AssetExists() {
+        assertTrue("Testing whether the asset for character 3 exists", Gdx.files.internal(Play.CHAR3).exists());
+    }
+}
