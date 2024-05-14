9110e8d0a9cc6ee13c359d4df48be32b4b81f4cc
TheRealEmissions <unitedrealm@hotmail.com>
2024-05-14 19:31:15 +0100

    player characters added to ImageAssets

--- game/core/src/com/eng1/game/assets/images/ImageAssets.java
+++ game/core/src/com/eng1/game/assets/images/ImageAssets.java
@@ -10,7 +10,10 @@ import java.util.function.Supplier;
 
 public enum ImageAssets implements Assets<Texture> {
     MAIN_MENU_TITLE(() -> new Texture(Gdx.files.internal("images/main_menu_title.png"))),
-    NEW_WORLD_MAP_OVERVIEW(() -> new Texture(Gdx.files.internal("maps/newWorldMap/newWorldMap.png")));
+    NEW_WORLD_MAP_OVERVIEW(() -> new Texture(Gdx.files.internal("maps/newWorldMap/newWorldMap.png"))),
+    PLAYER_CHARACTER_1(() -> new Texture(Gdx.files.internal("playerCharacters/playerCharacter1.png"))),
+    PLAYER_CHARACTER_2(() -> new Texture(Gdx.files.internal("playerCharacters/playerCharacter2.png"))),
+    PLAYER_CHARACTER_3(() -> new Texture(Gdx.files.internal("playerCharacters/playerCharacter3.png")));
 
     private final List<Texture> loadedTextures = new ArrayList<>();
     private final Supplier<Texture> texture;
