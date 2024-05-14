9ceec625859f170a581483dc47dbed92a5fea79c
therealemissions <me@liammartin.site>
2024-05-14 03:43:54 +0100

    forgot to annotate

--- game/core/src/com/eng1/game/assets/maps/MapLoader.java
+++ game/core/src/com/eng1/game/assets/maps/MapLoader.java
@@ -1,7 +1,9 @@
 package com.eng1.game.assets.maps;
 
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public final class MapLoader {
     private static final TmxMapLoader mapLoader = new TmxMapLoader();
 
