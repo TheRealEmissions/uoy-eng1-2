cc886c61bb77bc3161e78b30c3c986cd2b8725a7
TheRealEmissions <me@liammartin.site>
2024-05-15 02:31:25 +0100

    removed weird spacing

--- game/core/src/com/eng1/game/screens/PlayScreen.java
+++ game/core/src/com/eng1/game/screens/PlayScreen.java
@@ -86,7 +86,7 @@ public class PlayScreen implements Screen {
     }
     @Override
     public void render(float delta) {
-        Gdx.gl20.glClearColor( 0, 0, 0, 1 );
+        Gdx.gl20.glClearColor(0, 0, 0, 1);
         Gdx.gl20.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
         renderer.setView(camera);
         renderer.render();
