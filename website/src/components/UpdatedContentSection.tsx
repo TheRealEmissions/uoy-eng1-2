42b969287a43486659dee11239326ca07372efcf
lc2353 <120254125+lc2353@users.noreply.github.com>
2024-05-16 23:19:59 +0100

    updated website ready to insert updated documents

--- website/src/components/UpdatedContentSection.tsx
+++ website/src/components/UpdatedContentSection.tsx
@@ -30,7 +30,7 @@ const NewHeroSection = () => {
                         <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Updated Requirements Document</h5>
                             <p className="font-normal text-gray-700 pb-5">Here is the latest version of our requirements document.</p>
-                            <a className="cursor-pointer" href="updated_requirements.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/updated_requirements.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/updated_requirements.pdf</h6>
                             </a>
                         </div>
@@ -40,14 +40,14 @@ const NewHeroSection = () => {
                         <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Improved Architecture Overview</h5>
                             <p className="font-normal text-gray-700 pb-5">Check out the updated version of our architecture overview.</p>
-                            <a className="cursor-pointer" href="updated_architecture.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/updated_architecture.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/updated_architecture.pdf</h6>
                             </a>
                         </div>
                         <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Revised Method Selection and Planning</h5>
                             <p className="font-normal text-gray-700 pb-5">Here is the latest version of our method selection and planning document.</p>
-                            <a className="cursor-pointer" href="revised_methodselection.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/revised_methodselection.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/revised_methodselection.pdf</h6>
                             </a>
                         </div>
@@ -56,7 +56,7 @@ const NewHeroSection = () => {
                         <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Enhanced Risk Assessment and Mitigation</h5>
                             <p className="font-normal text-gray-700 pb-5 ">Explore the enhanced version of our risk assessment and mitigation document.</p>
-                            <a className="cursor-pointer" href="enhanced_riskassessment.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/enhanced_riskassessment.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/enhanced_riskassessment.pdf</h6>
                             </a>
                         </div>
@@ -64,7 +64,7 @@ const NewHeroSection = () => {
                             {/* <h6 className="text-sm ghosted">pdf</h6> */}
                             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 ">Updated Implementation Details</h5>
                             <p className="font-normal text-gray-700 pb-5">Check out the latest version of our implementation details.</p>
-                            <a className="cursor-pointer" href="updated_implementation.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/updated_implementation.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/updated_implementation.pdf</h6>
                             </a>
                         </div>
@@ -85,7 +85,7 @@ const NewHeroSection = () => {
                         <div className="block max-w-sm p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                             <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900 ">Updated Requirements Document</h5>
                             <p className="text-xs font-normal text-gray-700 pb-5">Here is the latest version of our requirements document.</p>
-                            <a className="cursor-pointer" href="updated_requirements.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/updated_requirements.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/updated_requirements.pdf</h6>
                             </a>
                         </div>
@@ -94,14 +94,14 @@ const NewHeroSection = () => {
                         <div className="block max-w-sm p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                             <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900 ">Improved Architecture Overview</h5>
                             <p className="text-xs font-normal text-gray-700 pb-5">Check out the updated version of our architecture overview.</p>
-                            <a className="cursor-pointer" href="updated_architecture.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/updated_architecture.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/updated_architecture.pdf</h6>
                             </a>
                         </div>
                         <div className="block max-w-sm p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                             <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900 ">Revised Method Selection <br></br> and Planning</h5>
                             <p className="text-xs font-normal text-gray-700 pb-5">Here is the latest version of our method selection and planning document.</p>
-                            <a className="cursor-pointer" href="revised_methodselection.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/revised_methodselection.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/revised_methodselection.pdf</h6>
                             </a>
                         </div>
@@ -110,7 +110,7 @@ const NewHeroSection = () => {
                         <div className="block max-w-sm p-5 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
                             <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900 ">Enhanced Risk Assessment <br></br> and Mitigation</h5>
                             <p className="text-xs font-normal text-gray-700 pb-5">Explore the enhanced version of our risk assessment and mitigation document.</p>
-                            <a className="cursor-pointer" href="enhanced_riskassessment.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/enhanced_riskassessment.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/enhanced_riskassessment.pdf</h6>
                             </a>
                         </div>
@@ -118,7 +118,7 @@ const NewHeroSection = () => {
                             {/* <h6 className="text-sm ghosted">pdf</h6> */}
                             <h5 className="mb-2 text-md font-bold tracking-tight text-gray-900 ">Updated Implementation Details</h5>
                             <p className="text-xs font-normal text-gray-700 pb-5">Check out the latest version of our implementation details.</p>
-                            <a className="cursor-pointer" href="updated_implementation.pdf" target="_blank" rel="noopener noreferrer" download>
+                            <a className="cursor-pointer" href="/updated_deliverables/updated_implementation.pdf" target="_blank" rel="noopener noreferrer" download>
                                 <h6 className="text-sm ghosted text-blue-500">/updated_implementation.pdf</h6>
                             </a>
                         </div>
