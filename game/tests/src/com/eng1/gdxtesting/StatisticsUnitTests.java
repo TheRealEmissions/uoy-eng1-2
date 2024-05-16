9701bc2f2fa08e51a2807d80b85d5eee0a2267d9
TheRealEmissions <me@liammartin.site>
2024-05-16 20:01:48 +0100

    unit test refactor

--- game/tests/src/com/eng1/gdxtesting/StatisticsUnitTests.java
+++ game/tests/src/com/eng1/gdxtesting/StatisticsUnitTests.java
@@ -2,70 +2,70 @@ package com.eng1.gdxtesting;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import com.eng1.game.game.player.GameStats;
+import com.eng1.game.game.player.Statistics;
 import java.time.LocalTime;
 
 // Be aware that when adding tests you may need to clean and rebuild
 
-public class GameStatsUnitTests {
+public class StatisticsUnitTests {
 
     @Test
     public void GameStatsIncreaseTime1HourTest() {
-        LocalTime intialTime = GameStats.getTime();
-        GameStats.increaseTime(LocalTime.of(1, 0));
+        LocalTime intialTime = Statistics.getTime();
+        Statistics.increaseTime(LocalTime.of(1, 0));
 
         assertEquals(
                 "Checks whether GameStats Increase Time method works as expected when 1 hour is added",
                 intialTime.plusHours(1),
-                GameStats.getTime()
+                Statistics.getTime()
         );
     }
 
     @Test
     public void GameStatsIncreaseTime15MinuteTest() {
-        LocalTime intialTime = GameStats.getTime();
-        GameStats.increaseTime(LocalTime.of(0, 15));
+        LocalTime intialTime = Statistics.getTime();
+        Statistics.increaseTime(LocalTime.of(0, 15));
 
         assertEquals(
                 "Checks whether GameStats Increase Time method works as expected when 15 minutes is added",
                 intialTime.plusMinutes(15),
-                GameStats.getTime()
+                Statistics.getTime()
         );
     }
 
     @Test
     public void GameStatsIncreaseTimeHoursAndMinutesTest() {
-        LocalTime intialTime = GameStats.getTime();
-        GameStats.increaseTime(LocalTime.of(2, 48));
+        LocalTime intialTime = Statistics.getTime();
+        Statistics.increaseTime(LocalTime.of(2, 48));
 
         assertEquals(
                 "Checks whether GameStats Increase Time method works as expected when 2:48 is added",
                 intialTime.plusHours(2).plusMinutes(48),
-                GameStats.getTime()
+                Statistics.getTime()
         );
     }
 
     @Test
     public void GameStatsDecreaseEnergyTest() {
-        int initialEnergy = GameStats.getEnergy();
-        GameStats.decreaseEnergy(1);
+        int initialEnergy = Statistics.getEnergy();
+        Statistics.decreaseEnergy(1);
 
         assertEquals(
                 "Checks that GameStats.decreaseEnergy() works as expected",
                 initialEnergy - 1,
-                GameStats.getEnergy()
+                Statistics.getEnergy()
         );
     }
 
     @Test
     public void GameStatsIncreaseScoreTest() {
-        int initialScore = GameStats.getScore();
-        GameStats.increaseScore(15);
+        int initialScore = Statistics.getScore();
+        Statistics.increaseScore(15);
 
         assertEquals(
                 "Checks whether GameStats.increaseScore works as expected",
                 initialScore + 15,
-                GameStats.getScore()
+                Statistics.getScore()
         );
     }
 
@@ -77,75 +77,75 @@ public class GameStatsUnitTests {
      */
     @Test
     public void GameStatsNewDayDayChangeTest() {
-        int intialDay = GameStats.getDay();
+        int intialDay = Statistics.getDay();
 
-        GameStats.newDay();
+        Statistics.newDay();
 
         assertEquals(
                 "Checks that GameStats.newDay() increases the day by 1",
                 intialDay + 1,
-                GameStats.getDay()
+                Statistics.getDay()
         );
     }
 
     @Test
     public void GameStatsNewDayTimeChangeTest() {
         // Ensure initialTime != DAY_START
-        if(GameStats.getTime().equals(GameStats.DAY_START)) {
-            GameStats.increaseTime(LocalTime.of(0, 5));
+        if(Statistics.getTime().equals(Statistics.DAY_START)) {
+            Statistics.increaseTime(LocalTime.of(0, 5));
         }
-        LocalTime initialTime = GameStats.getTime();
+        LocalTime initialTime = Statistics.getTime();
 
-        GameStats.newDay();
+        Statistics.newDay();
 
         assertEquals(
                 "Checks that GameStats.newDay() sets the time to DAY_START",
-                GameStats.DAY_START,
-                GameStats.getTime()
+                Statistics.DAY_START,
+                Statistics.getTime()
         );
     }
 
     @Test
     public void GameStatsNewDayEnergyChangeTest() {
         // Ensure initialEnergy != MAX_ENERGY
-        if (GameStats.getEnergy() == GameStats.MAX_ENERGY) {
-            GameStats.decreaseEnergy(1);
+        if (Statistics.getEnergy() == Statistics.MAX_ENERGY) {
+            Statistics.decreaseEnergy(1);
         }
-        int initialEnergy = GameStats.getEnergy();
+        int initialEnergy = Statistics.getEnergy();
 
-        GameStats.newDay();
+        Statistics.newDay();
 
         assertEquals(
                 "Checks that GameStats.newDay() sets the energy to MAX_ENERGY",
-                GameStats.MAX_ENERGY,
-                GameStats.getEnergy()
+                Statistics.MAX_ENERGY,
+                Statistics.getEnergy()
         );
     }
 
     @Test
     public void GameStatsNewDayUnchangedScoreTest() {
-        int initialScore = GameStats.getScore();
+        int initialScore = Statistics.getScore();
 
-        GameStats.newDay();
+        Statistics.newDay();
 
         assertEquals(
                 "Checks that GameStats.newDay() has no effect on an unchanged score",
                 initialScore,
-                GameStats.getScore()
+                Statistics.getScore()
         );
     }
 
     @Test
     public void GameStatsNewDayChangedScoreTest() {
-        GameStats.increaseScore(1);
-        int initialScore = GameStats.getScore();
+        Statistics.increaseScore(1);
+        int initialScore = Statistics.getScore();
 
-        GameStats.newDay();
+        Statistics.newDay();
 
         assertEquals(
                 "Checks that GameStats.newDay() has no effect on a changed score",
                 initialScore,
-                GameStats.getScore()
+                Statistics.getScore()
         );
     }
 
