1d2cd1b1b2dac2a54716a4198ba2784c080fa9c9
lc2353 <120254125+lc2353@users.noreply.github.com>
2024-05-22 16:13:30 +0100

    Revert "trying to delpoy website"
    This reverts commit 378338bc84a4097cd58f690f6ac539b4b5323c28.

--- website/next.config.js
+++ website/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: './',
-    trailingSlash: true,
-}
-
-module.exports = nextConfig;
+    }
+    
+    module.exports = nextConfig
