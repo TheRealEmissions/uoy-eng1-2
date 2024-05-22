daf7c1ffb65939bfcd5d25ba12e7ed5f488f7455
lc2353 <120254125+lc2353@users.noreply.github.com>
2024-05-22 23:41:56 +0100

    Added new figures

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/uoy-eng1-2',
+    assetPrefix: 'https://therealemissions.github.io/uoy-eng1-2/',
     images: {
         unoptimized: true,
     },
     output: 'export',
 
 }
-export default nextConfig;
+module.exports = nextConfig
--- src/components/UpdatedFigures.tsx
+++ src/components/UpdatedFigures.tsx
@@ -4,9 +4,23 @@ import React from 'react';
 
 const Figures = () => {
   return (
-<section id="Diagram section" className="xl:p-24 md:p-24 lg:p-24 p-5 flex justify-center items-center ">
+      <section id="Diagram section" className="xl:p-24 md:p-24 lg:p-24 p-5 flex justify-center items-center ">
+        <div className="flex flex-col md:flex-row gap-10 md:gap-52 pt-16">
+          <div>
+            <p className="text-sm">Figure 1</p>
+            <h3 className="text-xl md:text-2xl font-semibold">
+              Architecture
+            </h3>
+            <div className="max-w-xl text-sm">
+              <h2 className="text-xl">Updated Class Diagram</h2>
 
-    </section>
+            </div>
+          </div>
+          <div className="max-w-xl min-w-xl md:min-w-xl">
+            <img src="/updated_deliverables/New%20Class%20Diagram.jpg" className="rounded-xl w-full"/>
+          </div>
+        </div>
+      </section>
   );
 };
 
