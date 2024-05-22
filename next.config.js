1e6dfa0ab960c5259faa7d8282418e04f679248b
lc2353 <120254125+lc2353@users.noreply.github.com>
2024-05-22 23:04:59 +0100

    guessing at this point

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/uoy-eng1-2',
+    assetPrefix: 'https://therealemissions.github.io/uoy-eng1-2/',
     output: 'export',
+
 }
     
     module.exports = nextConfig
