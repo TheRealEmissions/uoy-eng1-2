de3952e13b1935436e9132d1174bed740745bdc9
therealemissions <me@liammartin.site>
2024-05-23 05:52:44 +0100

    assets interface

--- game/core/src/com/eng1/game/assets/Assets.java
+++ game/core/src/com/eng1/game/assets/Assets.java
@@ -2,6 +2,15 @@ package com.eng1.game.assets;
 
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This interface defines the methods that must be implemented by all asset managers, which in this game are enums.
+ * The type of asset is represented by the generic parameter {@link T}.
+ *
+ * The interface includes methods for getting an instance of the asset {@link #get()}, disposing of all instances of the asset {@link #dispose()},
+ * and disposing of a specific instance of the asset {@link #dispose(T)}.
+ *
+ * @param <T> The type of asset that the implementing class represents.
+ */
 public interface Assets<T> {
     T get();
     void dispose();
