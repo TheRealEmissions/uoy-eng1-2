b23432920ab6b2259ae2c2f00dc562dd8e0ec9d6
therealemissions <me@liammartin.site>
2024-05-23 08:43:18 +0100

    fixed this test file for some reason when there's an outstanding pr

--- game/tests/src/com/eng1/gdxtesting/AssetTests.java
+++ game/tests/src/com/eng1/gdxtesting/AssetTests.java
@@ -2,6 +2,7 @@ package com.eng1.gdxtesting;
 
 import static org.junit.Assert.assertTrue;
 
+import com.eng1.game.assets.images.ImageAssets;
 import com.eng1.game.game.player.Player;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,16 +13,16 @@ import com.badlogic.gdx.Gdx;
 public class AssetTests {
     @Test
     public void testCharacter1AssetExists() {
-        assertTrue("Testing whether the asset for character 1 exists", Gdx.files.internal(Player.CHAR1).exists());
+        assertTrue("Testing whether the asset for character 1 exists", ImageAssets.PLAYER_CHARACTER_1.get() != null);
     }
 
     @Test
     public void testCharacter2AssetExists() {
-        assertTrue("Testing whether the asset for character 2 exists", Gdx.files.internal(Player.CHAR2).exists());
+        assertTrue("Testing whether the asset for character 2 exists", ImageAssets.PLAYER_CHARACTER_2.get() != null);
     }
 
     @Test
     public void testCharacter3AssetExists() {
-        assertTrue("Testing whether the asset for character 3 exists", Gdx.files.internal(Player.CHAR3).exists());
+        assertTrue("Testing whether the asset for character 3 exists", ImageAssets.PLAYER_CHARACTER_3.get() != null);
     }
 }
