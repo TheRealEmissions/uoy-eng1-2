3b54546d8d500c9991d1ac7f2e8e06305f0d557d
therealemissions <me@liammartin.site>
2024-05-23 06:21:43 +0100

    dreamweaver docs

--- game/core/src/com/eng1/game/game/achievement/DreamweaverAchievement.java
+++ game/core/src/com/eng1/game/game/achievement/DreamweaverAchievement.java
@@ -3,6 +3,12 @@ package com.eng1.game.game.achievement;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Links with {@link Achievements#DREAMWEAVER}
+ * This achievement is achieved by daydreaming more than 4 times a week.
+ *
+ * The class is annotated with @Setter and @Getter from the Lombok library, which automatically generates getter and setter methods for its fields.
+ */
 @Setter
 @Getter
 public class DreamweaverAchievement implements Achievement {
@@ -11,17 +17,41 @@ public class DreamweaverAchievement implements Achievement {
     //(Daydream)
     //2%
 
+    /**
+     * The total number of times the player has daydreamed in a week.
+     *
+     * This field is used to track the progress towards achieving the {@link Achievements#DREAMWEAVER} achievement.
+     * The Dreamweaver achievement is achieved when the player has daydreamed more than 4 times in a week.
+     */
     private int total = 0;
 
+    /**
+     * Checks if the achievement has been achieved.
+     *
+     * The achievement is achieved when the player has daydreamed more than 4 times in a week.
+     * The method returns true if the total number of times the player has daydreamed in a week is greater than 4, and false otherwise.
+     *
+     * @return A boolean indicating whether the achievement has been achieved.
+     */
     @Override
     public boolean hasAchieved() {
         return total > 4;
     }
 
+    /**
+     * Increments the total number of times the player has daydreamed in a week.
+     *
+     * This method should be called when the player daydreams.
+     * Each call to this method represents one instance of daydreaming.
+     * The total field is incremented by one each time this method is called.
+     */
     public void daydream() {
         total++;
     }
 
+    /**
+     * Resets the total number of times the player has daydreamed in a week.
+     */
     public void reset() {
         total = 0;
     }
